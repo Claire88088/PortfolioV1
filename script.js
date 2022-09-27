c5752a7789654524122e4a51b81a34a2243b41f5
Claire <claire_bouton@hotmail.com>
2022-09-27 14:27:32 +0200

    ajout menu collant et responsive

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var gotopBtn = document.getElementById('gotop-btn');
+var barsIcon = document.getElementById('bars-icon');
+var responsiveMenu = document.getElementById('main-nav--responsive');
 // console.log(gotopBtn, 'gotop btn')
+// console.log(barsIcon, 'bars icon');
+// console.log(responsiveMenu, 'menu responsive');
+
+function removeResponsiveMenu() {
+    responsiveMenu.style.transition = '2s';
+    responsiveMenu.style.transform = 'translateX(-200px)';  
+}
+
 
 // gestion du bouton de retour vers haut de page
 window.onscroll = ( _ => {
@@ -10,5 +20,22 @@ window.onscroll = ( _ => {
 
 gotopBtn.onclick = ( _ => {
     gotopBtn.style.display = 'none';
+});
+
+// gestion du menu responsive
+barsIcon.onclick = ( _ => {
+    if (responsiveMenu.style.transform !== 'translateX(0px)') {
+        // barsIcon.classList.remove('fa-bars');
+        // barsIcon.classList.add('fa-arrow-left');
+        responsiveMenu.style.transform = 'translateX(0px)';  
+    } else {
+        // barsIcon.classList.add('fa-bars');
+        // barsIcon.classList.remove('fa-arrow-left');
+        removeResponsiveMenu();
+    }
+});
+
+responsiveMenu.onclick = ( _ => {
+    removeResponsiveMenu();
 });
 
